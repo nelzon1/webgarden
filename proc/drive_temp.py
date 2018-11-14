e6ae99ee47d8310dac29f1c840ab3845a9534dfd
Jake Nelson <jake.nelzon@gmail.com>
2018-11-13 22:52:50 -0800

    slower temp capture and seconds in timestamp

--- proc/drive_temp.py
+++ proc/drive_temp.py
@@ -4,7 +4,7 @@ import os
 
 while True:
     now = time.localtime()
-    timestr = str(now[0]) + '-' + str(now[1]).zfill(2) + '-' + str(now[2]).zfill(2) + ' ' + str(now[3]).zfill(2) + ':' + str(now[4]).zfill(2)
+    timestr = str(now[0]) + '-' + str(now[1]).zfill(2) + '-' + str(now[2]).zfill(2) + ' ' + str(now[3]).zfill(2) + ':' + str(now[4]).zfill(2) + ':' + str(now[5]).zfill(2)
     datestamp = str(now[0]) + '-' + str(now[1]).zfill(2) + '-' + str(now[2]).zfill(2)
     curTemp = temp.read_temp()
     #import pdb; pdb.set_trace()
@@ -14,4 +14,4 @@ while True:
             print(datum[:-1])
     except:
         print('File write error')
-    time.sleep(15)
+    time.sleep(60)
