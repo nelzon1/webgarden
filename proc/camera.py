a8b1564a19adc20a6bed55d2d4d29e510f101ad8
Jake Nelson <jake.nelzon@gmail.com>
2018-11-13 22:07:58 -0800

    Fixing camera directories

--- proc/camera.py
+++ proc/camera.py
@@ -1,6 +1,7 @@
 from picamera import PiCamera
 from time import sleep
 import subprocess
+import os
 
 camera =PiCamera()
 camera.resolution = (1000,1000)
@@ -9,11 +10,12 @@ def snap(filename, text):
     camera.annotate_text = text
     camera.start_preview()
     sleep(2)
+    import pdb; pdb.set_trace()
     try:
         camera.capture(filename)
     except IOError:
         print("directory not found, trying to create...")
-        subprocess.run("mkdir " + filename[:-17])
+        subprocess.call(["mkdir",filename[:-17]])
         camera.capture(filename)
 
     camera.stop_preview()
