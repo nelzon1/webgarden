1d82d5c64922edd902081abf304d16ac3905e977
Jake Nelson <jake.nelzon@gmail.com>
2018-11-13 21:45:22 -0800

    fixed camera write when missing directory

--- proc/camera.py
+++ proc/camera.py
@@ -1,17 +1,23 @@
 from picamera import PiCamera
 from time import sleep
+import subprocess
 
 camera =PiCamera()
-camera.resolution = (500,500)
+camera.resolution = (1000,1000)
 
 def snap(filename, text):
+    camera.annotate_text = text
+    camera.start_preview()
+    sleep(2)
     try:
-        camera.annotate_text = text
-        camera.start_preview()
-        sleep(2)
         camera.capture(filename)
-        camera.stop_preview()
-    except:
-        raise
+    except IOError:
+        print("directory not found, trying to create...")
+        subprocess.run("mkdir " + filename[:-17])
+        camera.capture(filename)
+
+    camera.stop_preview()
+    print("Image saved: " + filename)
+
 
     return 1
