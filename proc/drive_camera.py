42869c0e4d58f48140e5143698d2c4746d993e52
Jake Nelson <jake.nelzon@gmail.com>
2019-04-14 11:57:23 -0700

    changed write methods to use db and added creds

--- proc/drive_camera.py
+++ proc/drive_camera.py
@@ -2,10 +2,35 @@ import camera
 import time
 import os
 
+import json
+import mysql.connector
+
+global cred
+
+with open('dbinfo.json','r') as json_file:
+    cred = json.load(json_file)
+
+mydb = mysql.connector.connect(
+        host=cred['connection']['host'],
+        user=cred['connection']['user'],
+        passwd=cred['connection']['pwd'],
+        database=cred['connection']['database'],
+        port=cred['connection']['port']
+        )
+
+mycursor = mydb.cursor()
+
+def writeImagePath(image_path, timeStr):
+    sql = "INSERT INTO IMAGES VALUES (%s,%s)"
+    val = (image_path,timeStr)
+    mycursor.execute(sql,val)
+
 while True:
     now = time.localtime()
-    timestr = str(now[1]) + '/' + str(now[2]) + '/' + str(now[0]) + ' - ' + str(now[3]) + ':' + str(now[4])
+    timeStr = str(now[1]) + '/' + str(now[2]) + '/' + str(now[0]) + ' - ' + str(now[3]) + ':' + str(now[4])
     timestamp = str(now[0]) + str(now[1]).zfill(2) + str(now[2]).zfill(2) + str(now[3]).zfill(2) + str(now[4]).zfill(2)
     datestamp = str(now[0]) + '-' + str(now[1]).zfill(2) + '-' + str(now[2]).zfill(2)
-    camera.snap(os.path.join("..","dash","static","dash","images", datestamp ,timestamp + '.jpg'), timestr)
+    image_path = os.path.join("..","dash","static","dash","images", datestamp ,timestamp + '.jpg')
+    camera.snap(image_path, timeStr)
+    writeImagePath(image_path, timeStr)
     time.sleep(300)
