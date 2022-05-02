59b10f7eb9b26abc85f3eef028e9157fa62f66fa
jnelson <jake.nelzon@gmail.com>
2022-05-02 10:05:43 -0700

    add commit to temp sql

--- proc/getTemp.py
+++ proc/getTemp.py
@@ -1,16 +1,15 @@
 import temperature as temp
 import time
 import os
-import json
 import sqlite3
 
 dbconn = sqlite3.connect('/home/piserver/dev/webgarden/appDB.sqlite3')
-mycursor = dbconn.cursor()
+dbcurr = dbconn.cursor()
 
 def writeTemp(tempData, timeStr):
-    sql = "INSERT INTO Temperature (Temperature,Datetime) VALUES (?,'?')"
+    sql = "INSERT INTO Temperature (Temperature,Datetime) VALUES (?,?)"
     val = (tempData,timeStr)
-    mycursor.execute(sql,val)
+    dbcurr.execute(sql,val)
 
 now = time.localtime()
 timeStr = str(now[0]) + '-' + str(now[1]).zfill(2) + '-' + str(now[2]).zfill(2) + ' ' + str(now[3]).zfill(2) + ':' + str(now[4]).zfill(2) + ':' + str(now[5]).zfill(2)
@@ -19,5 +18,6 @@ curTemp = temp.read_temp()[0]
 #import pdb; pdb.set_trace()
 print("Current temperature: ", curTemp, timeStr)
 writeTemp(curTemp, timeStr)
+dbcurr.commit()
 dbconn.close()
 
