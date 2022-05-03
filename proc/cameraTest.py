2a500141392390eab0132a870084b32066f1b1c4
jnelson <jake.nelzon@gmail.com>
2022-05-02 23:35:16 -0700

    update camera

--- proc/cameraTest.py
+++ proc/cameraTest.py
@@ -2,11 +2,9 @@ import cv2 as cv
 
 cap = cv.VideoCapture('/dev/video0', cv.CAP_V4L)
 
-fps = cap.get(cv.CAP_PROP_FPS)
-
 # set dimensions
 cap.set(cv.CAP_PROP_FRAME_WIDTH, 800)
-cap.set(cv.CAP_PROP_FRAME_HEIGHT, 400)
+cap.set(cv.CAP_PROP_FRAME_HEIGHT, 600)
 
 def snapshot(filename):
     ret,frame = cap.read()
@@ -14,7 +12,7 @@ def snapshot(filename):
         cv.imwrite(filename,frame)
         print('success. image saved at: ' + filename)
 
-snapshot('test2022.jpg')
+snapshot('LatestImage.jpg')
 
 # release camera 
 cap.release()
