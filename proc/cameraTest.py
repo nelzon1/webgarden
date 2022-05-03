4f77daa58b8e1d111c29790e0e4a483a82c9d5f7
jnelson <jake.nelzon@gmail.com>
2022-05-02 23:37:36 -0700

    fix

--- proc/cameraTest.py
+++ proc/cameraTest.py
@@ -4,7 +4,7 @@ cap = cv.VideoCapture('/dev/video0', cv.CAP_V4L)
 
 # set dimensions
 cap.set(cv.CAP_PROP_FRAME_WIDTH, 800)
-cap.set(cv.CAP_PROP_FRAME_HEIGHT, 800)
+cap.set(cv.CAP_PROP_FRAME_HEIGHT, 500)
 
 def snapshot(filename):
     ret,frame = cap.read()
